481e3f1d23b4fa860cdb02a41cd681019651f78b
Urielthebane <oluwankorinolaa@gmail.com>
2025-11-27 23:53:35 +0100

    fix port  error

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def ask():
     return jsonify({"answer": answer})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("port", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
